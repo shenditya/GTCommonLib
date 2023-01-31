0540bf968420536f318d84213889c5142480e2d0
Shendy <shendyaditya@live.com>
2023-01-31 12:05:49 +0700

    update font manager

--- GTEPCommonLib/Classes/FontsManager.swift
+++ GTEPCommonLib/Classes/FontsManager.swift
@@ -13,6 +13,24 @@ extension UIFont {
     
     private class FontsManager {}
     
+    static func loadFontWith(name: String) {
+        let frameworkBundle = Bundle(for: FontsManager.self)
+        if let pathForResourceString = frameworkBundle.path(forResource: name, ofType: "ttf") {
+            let fontData = NSData(contentsOfFile: pathForResourceString)
+            let dataProvider = CGDataProvider(data: fontData!)
+            let fontRef = CGFont(dataProvider!)
+            var errorRef: Unmanaged<CFError>? = nil
+            
+            if (CTFontManagerRegisterGraphicsFont(fontRef!, &errorRef) == false) {
+                NSLog("Failed to register font - register graphics font failed - this font may have already been registered in the main bundle.")
+            }
+        }else{
+            NSLog("Font \(name) Not Found On Library")
+        }
+        
+        
+    }
+    
     ///Use PostScript Name as font Family name.
     ///Make sure file extension is correct (ttf, otf)
     ///name: "Roboto-Bold", ofType: "ttf", identifier: Bundle.main.bundleIdentifier
@@ -36,4 +54,16 @@ extension UIFont {
         }
 
     }
+    
+    ///Font 1: "Roboto-Bold"
+    ///Font 1: "Roboto-Light"
+    ///Font 1: Roboto-Medium"
+    ///Font 1: "Roboto-Regular"
+    public static let loadMyFonts: () = {
+        loadFontWith(name: "Roboto-Bold")
+        loadFontWith(name: "Roboto-Light")
+        loadFontWith(name: "Roboto-Medium")
+        loadFontWith(name: "Roboto-Regular")
+      
+    }()
 }
